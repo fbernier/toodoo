8a338ce3d487a2979e7df298e17f021129c8474a
Francois Bernier <frankbernier@gmail.com>
2012-03-27 19:07:24 -0400

    Added initial gem configuration setup

--- lib/toodoo.rb
+++ lib/toodoo.rb
@@ -3,6 +3,22 @@ require 'term/ansicolor'
 require 'date'
 
 module Toodoo
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :priority_color
+
+    def initialize
+      @priority_color = { :low => Color.yellow, :medium => Color.orange, :high => Color.red }
+    end
+  end
 end
 
 class Color
@@ -12,7 +28,7 @@ end
 module Kernel
   def toodoo(description, priority = :low, due_date = Date.today)
     if due_date >= Date.today
-      print Color.red, Color.bold, "TODO: " << description, "\n", caller[0], Color.clear, "\n"
+      print Toodoo.Configuration.priority_color[:priority], Color.bold, "TODO: " << description, "\n", caller[0], Color.clear, "\n"
     end
 
     yield unless block_given?
